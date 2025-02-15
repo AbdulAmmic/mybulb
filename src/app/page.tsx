cbd6f96fd9ee39f5c27ff73d1e9687b7a9d65d4c
AbdulAmmic <connectorstech@gmail.com>
2025-02-15 16:04:01 +0100

    hh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Lightbulb, LightbulbOff } from 'lucide-react';
 
 const BulbControl = () => {
     const [status, setStatus] = useState<string>('OFF');
-    const apiUrl = 'https://8891-102-91-105-211.ngrok-free.app'; 
+    const apiUrl = 'https://6ed3-102-91-105-211.ngrok-free.app'; 
 
     const fetchStatus = async () => {
         try {
